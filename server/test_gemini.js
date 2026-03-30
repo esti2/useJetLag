3c429120e8e99032ccbaacc366e2c7e2540cd646
galraij <galraij@gmail.com>
2026-03-30 17:32:18 +0300

    refactor: update test_gemini.js to log full response data instead of model names

--- server/test_gemini.js
+++ server/test_gemini.js
@@ -4,8 +4,7 @@ const apiKey = process.env.GEMINI_API_KEY;
 async function run() {
   const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${apiKey}`);
   const data = await res.json();
-  const names = data.models.map(m => m.name);
-  console.log(names);
+  console.log(JSON.stringify(data, null, 2));
 }
 
 run();
